cdbb66b5d3fa78805eb547a530003535435187b1
ateqaes <atequer.rahman@anwargroup.net>
2022-01-23 18:14:50 +0600

    initial commit.

---
+++ src/main/java/com/aes/corebackend/AescorebackendApplication.java
@@ -0,0 +1,13 @@
+package com.aes.corebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AescorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AescorebackendApplication.class, args);
+	}
+
+}
