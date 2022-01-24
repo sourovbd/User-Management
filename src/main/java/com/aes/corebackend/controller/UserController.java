7e6744aa657c9e9e4ad57e8ef4bad3528ee0a8cc
ateqaes <atequer.rahman@anwargroup.net>
2022-01-24 16:00:12 +0600

    annotation updated from Controller to RestController.

--- src/main/java/com/aes/corebackend/controller/UserController.java
+++ src/main/java/com/aes/corebackend/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.aes.corebackend.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
+    @GetMapping("/")
+    public String hello() {
+
+        return "Hello AES";
+    }
+
+
 }
