6b61f3a6d074b799e5a5ca03b7f362a9ce40fa0c
ateqaes <atequer.rahman@anwargroup.net>
2022-01-27 11:07:09 +0600

    One missing service is added & spring security code commented out.

--- src/main/java/com/aes/corebackend/controller/UserController.java
+++ src/main/java/com/aes/corebackend/controller/UserController.java
@@ -27,6 +27,9 @@ public class UserController {
     @Autowired
     private UserCredentialService userCredentialService;
 
+    @Autowired
+    private UserCredentialService userCredentialService;
+
     @PostMapping("users/save-credential")
     public ResponseEntity<?> saveCredential(@RequestBody UserCredentialDTO userCredentialDTO) {
         UserCredential userCredential = userCredentialDTO.to(userCredentialDTO);
