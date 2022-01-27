ea9cd9cd8b528495b23703843f023455f6479e7c
ateqaes <atequer.rahman@anwargroup.net>
2022-01-27 12:40:58 +0600

    spring security jwt code added.

--- src/main/java/com/aes/corebackend/config/SecurityConfigurer.java
+++ src/main/java/com/aes/corebackend/config/SecurityConfigurer.java
@@ -1,8 +1,19 @@
 package com.aes.corebackend.config;
 
 
+import com.aes.corebackend.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-/*@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -12,4 +23,23 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
-}*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
--- src/main/java/com/aes/corebackend/controller/AuthenticateController.java
+++ src/main/java/com/aes/corebackend/controller/AuthenticateController.java
@@ -1,26 +1,39 @@
 package com.aes.corebackend.controller;
 
+import com.aes.corebackend.dto.AuthenticationRequest;
+import com.aes.corebackend.dto.AuthenticationResponse;
+import com.aes.corebackend.service.MyUserDetailsService;
+import com.aes.corebackend.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticateController {
 
-   /* @Autowired
-    private AuthenticationManager authenticationManager;*/
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    /*@Autowired
-    private MyUserDetailsService userDetailsService;*/
+    @Autowired
+    private MyUserDetailsService userDetailsService;
 
-    /*@Autowired
-    private JwtUtil jwtTokenUtil;*/
+    @Autowired
+    private JwtUtil jwtTokenUtil;
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello AES.";
+    public ResponseEntity<?> hello() {
+
+        return ResponseEntity.ok("Hello AES.");
     }
-    /*@PostMapping("/authenticate")
+
+    @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
         try{
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getPassword(), authenticationRequest.getPassword()));
@@ -30,6 +43,6 @@ public class AuthenticateController {
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
         final String jwt = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new AuthenticationResponse(jwt));
-    }*/
+    }
 
 }
