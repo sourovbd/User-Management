be8e8cf3f2b40d8109436abfc86ff6cfe2919195
ateqaes <atequer.rahman@anwargroup.net>
2022-01-30 18:26:35 +0600

    Role based authorization is implemented.

--- src/main/java/com/aes/corebackend/controller/UserController.java
+++ src/main/java/com/aes/corebackend/controller/UserController.java
@@ -9,6 +9,7 @@ import com.aes.corebackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
@@ -27,6 +28,7 @@ public class UserController {
     private UserCredentialService userCredentialService;
 
     @PostMapping("users/save-password")
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<?> saveCredential(@RequestBody UserCredentialDTO userCredentialDTO) {
         UserCredential userCredential = userCredentialDTO.to(userCredentialDTO);
         boolean saved = userCredentialService.save(userCredential);
@@ -39,6 +41,7 @@ public class UserController {
     }
         
     @PostMapping("/user/create")
+    @PreAuthorize("hasRole('SYS_ADMIN')")
     public ResponseEntity<?> createUser(@RequestBody UserDTO userDto) {
         User user = userService.save(userDto.dtoToUser(userDto));
         if (Objects.nonNull(user)) {
@@ -50,22 +53,15 @@ public class UserController {
     }
     
     @PutMapping("/users/{id}")
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<?> updateUser(@RequestBody UserDTO userDto) {
-        //User user = userDto.dtoToUser(userDto);
-        //boolean success = userService.update(userDto.dtoToUser(userDto));
-
         return userService.update(userDto.dtoToUser(userDto))?
                 ResponseEntity.ok(new UserCreationResponseDTO("user data updated")) :
                 ResponseEntity.ok(new UserCreationResponseDTO("user update failed"));
-        /*if (success) {
-            //emailSender.send(userDto.dtoToUser(userDto).getEmailAddress(),"This is a test email");
-            return ResponseEntity.ok(new UserCreationResponseDTO("user data updated"));
-        }
-        else
-            return ResponseEntity.ok(new UserCreationResponseDTO("user update failed"));*/
     }
     
     @PostMapping("users/reset-password/{id}")
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<?> updateCredential(@RequestBody UserCredentialDTO userCredentialDTO, @PathVariable Long id) {
         UserCredential userCredential = userCredentialDTO.to(userCredentialDTO);
         boolean updated = userCredentialService.update(userCredential, id);
@@ -78,6 +74,7 @@ public class UserController {
     }
 
     @PostMapping("users/verify-credential")
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<?> verifyCredential(@RequestBody UserCredentialDTO userCredentialDTO) {
         boolean verified = userCredentialService.verifyPassword(userCredentialDTO);
         if (verified) {
@@ -89,12 +86,14 @@ public class UserController {
     }
     
     @GetMapping("get/user/{id}")
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<?> getUserDetails(@PathVariable int id) {
         User user = userService.findById(id).orElse(null);
         return ResponseEntity.ok(new UserFinderResponseDTO("use fetch ok",user));
     }
 
     @PostMapping("users/forgot-password")
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<?> forgotPassword(@RequestBody ForgotPasswordDTO forgotPasswordDTO) {
         //email validation
         boolean done = userCredentialService.generateAndSendTempPass(forgotPasswordDTO.getEmailAddress());
