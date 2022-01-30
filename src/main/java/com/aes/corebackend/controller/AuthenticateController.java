942967a128314acdf6d3816937e63cf3f5ff36b6
ateqaes <atequer.rahman@anwargroup.net>
2022-01-30 12:35:39 +0600

    Introduced logger.

--- src/main/java/com/aes/corebackend/controller/AuthenticateController.java
+++ src/main/java/com/aes/corebackend/controller/AuthenticateController.java
@@ -2,8 +2,11 @@ package com.aes.corebackend.controller;
 
 import com.aes.corebackend.dto.AuthenticationRequest;
 import com.aes.corebackend.dto.AuthenticationResponse;
-import com.aes.corebackend.service.MyUserDetailsService;
+import com.aes.corebackend.service.CustomUserDetailsService;
 import com.aes.corebackend.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,14 +18,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class AuthenticateController {
+
+    final Logger logger = LoggerFactory.getLogger(AuthenticateController.class);
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private MyUserDetailsService userDetailsService;
+    private CustomUserDetailsService userDetailsService;
 
     @Autowired
     private JwtUtil jwtTokenUtil;
@@ -36,10 +42,12 @@ public class AuthenticateController {
     @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
         try{
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getPassword(), authenticationRequest.getPassword()));
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
         } catch(BadCredentialsException e) {
              throw new Exception("Incorrect username or password.", e);
         }
+        logger.info("username: "+authenticationRequest.getUsername());
+        logger.info("password: "+authenticationRequest.getPassword());
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
         final String jwt = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new AuthenticationResponse(jwt));
