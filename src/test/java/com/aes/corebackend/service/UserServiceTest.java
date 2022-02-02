be49b2ab922740ce7672af9c0cf04d00b20c07ab
ahadulalam <ahad.alam@anwargroup.net>
2022-02-02 11:23:26 +0600

    email send functionality moved to service.code refactor

--- src/test/java/com/aes/corebackend/service/UserServiceTest.java
+++ src/test/java/com/aes/corebackend/service/UserServiceTest.java
@@ -1,18 +1,18 @@
 package com.aes.corebackend.service;
 
 import com.aes.corebackend.controller.UserController;
+import com.aes.corebackend.dto.ResponseDTO;
+import com.aes.corebackend.dto.UserDTO;
 import com.aes.corebackend.entity.User;
 import com.aes.corebackend.entity.UserCredential;
 import com.aes.corebackend.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
@@ -31,7 +31,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(MockitoJUnitRunner.class)
+
 public class UserServiceTest {
 
     @Autowired
@@ -53,15 +53,16 @@ public class UserServiceTest {
     UserCredential userCredential_1 = new UserCredential(1,"101","a1wq",true,"EMPLOYEE");
     UserCredential userCredential_2 = new UserCredential(2,"102","a1wq",true,"EMPLOYEE");
     UserCredential userCredential_3 = new UserCredential(3,"103","a1wq",true,"EMPLOYEE");
-    User user_1 = new User(1L,"abc@gmail.com","agm","101","a1polymar","accounts",userCredential_1);
-    User user_2 = new User(2L,"abd@gmail.com","agm","102","a1polymar","accounts",userCredential_2);
-    User user_3 = new User(3L,"abe@gmail.com","agm","103","a1polymar","accounts",userCredential_3);
+    User user_1 = new User(1L,"abc@gmail.com","agm","101","a1polymar","accounts","EMPLOYEE",userCredential_1);
+    User user_2 = new User(2L,"abd@gmail.com","agm","102","a1polymar","accounts","EMPLOYEE",userCredential_2);
+    User user_3 = new User(3L,"abe@gmail.com","agm","103","a1polymar","accounts","EMPLOYEE",userCredential_3);
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
         userCredentialService = Mockito.mock(UserCredentialService.class);
         mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        userService = Mockito.mock(UserService.class);
     }
     @Test
     public void createUserTest() throws Exception {
@@ -73,9 +74,22 @@ public class UserServiceTest {
         user.setEmailAddress("mdahad118@gmail.com");
         user.setBusinessUnit("a1polymar");
         user.setEmployeeId("0101");
+        user.setRoles("EMPLOYEE");
+        UserDTO userDto = new UserDTO();
+        userDto.setDesignation("agm");
+        userDto.setDepartment("accounts");
+        userDto.setEmailAddress("mdahad118@gmail.com");
+        userDto.setBusinessUnit("a1polymar");
+        userDto.setEmployeeId("0101");
+        userDto.setRoles("EMPLOYEE");
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setMessage("user created successfully");
+        responseDTO.setSuccess(true);
+        responseDTO.setData(null);
+
         user.setUserCredential(userCredential);
         Mockito.when(userRepository.save(user)).thenReturn(user);
-        //Mockito.when(userService.create(user)).thenReturn(user);
+        Mockito.when(userService.create(user,userDto)).thenReturn(responseDTO);
         // UserCreationResponseDTO response = om.rea  dValue(resultContent, UserCreationResponseDTO.class);
         // assertEquals(response.getMessage(),"user created");
     }
@@ -89,13 +103,13 @@ public class UserServiceTest {
                 .andExpect(status().isOk());
     }
     @Test
-    public void getUserByDetailsTest() throws Exception {
+    public void getUserDetailsTest() throws Exception {
         Mockito.when(userRepository.findById(1L)).thenReturn(Optional.ofNullable(user_1));
         Mockito.when(userService.read(1L)).thenReturn(user_1);
     }
     @Test
     public void updateUserById() throws Exception {
-        User user_1_temp = new User(1,"abc@gmail.com","dgm","101","a1polymar","accounts",userCredential_1);
+        User user_1_temp = new User(1,"abc@gmail.com","dgm","101","a1polymar","accounts","EMPLOYEE",userCredential_1);
         // Mockito.when(userService.update(user_1_temp,1L)).thenReturn(true);
     }
     /*@Autowired
