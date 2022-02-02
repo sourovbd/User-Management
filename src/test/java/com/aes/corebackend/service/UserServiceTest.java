77560cd8aa76997bbb1cdcd78f4d8d1bd391387e
ateqaes <atequer.rahman@anwargroup.net>
2022-02-02 16:51:11 +0600

    code refactor.

--- src/test/java/com/aes/corebackend/service/UserServiceTest.java
+++ src/test/java/com/aes/corebackend/service/UserServiceTest.java
@@ -38,12 +38,12 @@ public class UserServiceTest {
     private UserService userService;
 
     ObjectMapper om = new ObjectMapper();
-    UserCredential userCredential_1 = new UserCredential(1,"101","a1wq",true,"EMPLOYEE");
-    UserCredential userCredential_2 = new UserCredential(2,"102","a1wq",true,"EMPLOYEE");
-    UserCredential userCredential_3 = new UserCredential(3,"103","a1wq",true,"EMPLOYEE");
-    User user_1 = new User(1L,"abc@gmail.com","agm","101","a1polymar","accounts","EMPLOYEE",userCredential_1);
-    User user_2 = new User(2L,"abd@gmail.com","agm","102","a1polymar","accounts","EMPLOYEE",userCredential_2);
-    User user_3 = new User(3L,"abe@gmail.com","agm","103","a1polymar","accounts","EMPLOYEE",userCredential_3);
+    UserCredential userCredential_1 = createUserCredential(1,"101","a1wq",true,"EMPLOYEE");
+    UserCredential userCredential_2 = createUserCredential(2,"102","a1wq",true,"EMPLOYEE");
+    UserCredential userCredential_3 = createUserCredential(3,"103","a1wq",true,"EMPLOYEE");
+    User user_1 = createUser(1L,"abc@gmail.com","agm","101","a1polymar","accounts","EMPLOYEE",userCredential_1);
+    User user_2 = createUser(2L,"abd@gmail.com","agm","102","a1polymar","accounts","EMPLOYEE",userCredential_2);
+    User user_3 = createUser(3L,"abe@gmail.com","agm","103","a1polymar","accounts","EMPLOYEE",userCredential_3);
 
     @BeforeEach
     public void setup() {
@@ -110,5 +110,15 @@ public class UserServiceTest {
         Mockito.when(userService.update(user_1_temp,1L)).thenReturn(responseDTO);
         assertEquals(userService.update(user_1_temp,1L).getMessage(),responseDTO.getMessage());
         assertEquals(userService.update(user_1_temp,1L).getData(),responseDTO.getData());
+    }
+
+    public UserCredential createUserCredential(long id, String employeeId, String password, boolean flag, String roles) {
+
+        return new UserCredential(id, employeeId, password, flag, roles);
+    }
+
+    public User createUser(long id, String emailAddress, String designation, String employeeId, String businessUnit, String department, String roles, UserCredential userCredential) {
+
+        return new User(id, emailAddress, designation, employeeId, businessUnit, department, roles, userCredential);
     }
 }
