e50346bcd9e6366a4ffe52922802d104314e3fe6
AESCentral <hossainfurkaan@gmail.com>
2022-02-03 13:36:24 +0600

    familyDTO validate done

--- src/main/java/com/aes/corebackend/controller/personnelmanagement/FamilyInformationController.java
+++ src/main/java/com/aes/corebackend/controller/personnelmanagement/FamilyInformationController.java
@@ -1,12 +1,16 @@
 package com.aes.corebackend.controller.personnelmanagement;
 
 import com.aes.corebackend.dto.personnelmanagement.PersonalFamilyInfoDTO;
+import com.aes.corebackend.dto.personnelmanagement.PersonnelManagementResponseDTO;
 import com.aes.corebackend.service.personnelmanagement.FamilyInformationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,12 +18,18 @@ public class FamilyInformationController {
     private final FamilyInformationService familyInformationService;
 
     @PostMapping(value = "/users/{userId}/family-information")
-    public ResponseEntity<?> createFamilyInfo(@RequestBody PersonalFamilyInfoDTO familyInfoDTO, @PathVariable Long userId) {
+    public ResponseEntity<?> createFamilyInfo(@Valid @RequestBody PersonalFamilyInfoDTO familyInfoDTO, BindingResult result, @PathVariable Long userId) {
+        if(result.hasErrors()){
+            return ResponseEntity.ok(new PersonnelManagementResponseDTO(result.getFieldError().getDefaultMessage(), false, null));
+        }
         return ResponseEntity.ok(familyInformationService.create(familyInfoDTO, userId));
     }
 
     @PutMapping(value = "/users/{userId}/family-information")
-    public ResponseEntity<?> updateFamilyInfo(@RequestBody PersonalFamilyInfoDTO familyInfoDTO, @PathVariable Long userId) {
+    public ResponseEntity<?> updateFamilyInfo(@Valid @RequestBody PersonalFamilyInfoDTO familyInfoDTO, BindingResult result, @PathVariable Long userId) {
+        if(result.hasErrors()){
+            return ResponseEntity.ok(new PersonnelManagementResponseDTO(result.getFieldError().getDefaultMessage(), false, null));
+        }
         return ResponseEntity.ok(familyInformationService.update(familyInfoDTO, userId));
     }
 
--- src/main/java/com/aes/corebackend/dto/personnelmanagement/PersonalFamilyInfoDTO.java
+++ src/main/java/com/aes/corebackend/dto/personnelmanagement/PersonalFamilyInfoDTO.java
@@ -2,19 +2,30 @@ package com.aes.corebackend.dto.personnelmanagement;
 
 import com.aes.corebackend.entity.personnelmanagement.PersonalFamilyInfo;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Pattern;
 
 @Data
 public class PersonalFamilyInfoDTO {
     //enum
+    @Length(min = 0, max = 50, message = "Marital Status: Field must less than 50 characters")
+    @Pattern(regexp = "^[a-zA-z]+$", message = "Marital Status: Field cannot have numeric or special characters")
     private String maritalStatus;
     //a-zA-Z
     //max length 50
+    @Length(min = 0, max = 50, message = "Father's name: Field must less than 50 characters")
+    @Pattern(regexp = "^([a-zA-Z]+\\s)*[a-zA-Z]+$", message = "Father's name: Field cannot have numeric or special characters")
     private String fathersName;
     //a-zA-Z
     //max length 50
+    @Length(min = 0, max = 50, message = "Mother's Name: Field must less than 50 characters")
+    @Pattern(regexp = "^([a-zA-Z]+\\s)*[a-zA-Z]+$", message = "Mother's Name: Field cannot have numeric or special characters")
     private String mothersName;
     //a-zA-Z
     //max length 50
+    @Length(min = 0, max = 50, message = "Spouse's Name: Field must less than 50 characters")
+    @Pattern(regexp = "^([a-zA-Z]+\\s)*[a-zA-Z]+$", message = "Spouse's Name: Field cannot have numeric or special characters")
     private String spouseName;
 
     public static PersonalFamilyInfo getPersonalFamilyEntity(PersonalFamilyInfoDTO familyInfoDTO) {
