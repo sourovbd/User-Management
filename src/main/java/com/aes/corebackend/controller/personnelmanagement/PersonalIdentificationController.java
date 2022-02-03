f035f77b355b340fbe60695969c989af3c51eeb4
AESCentral <hossainfurkaan@gmail.com>
2022-02-03 14:32:54 +0600

    personal identification done

--- src/main/java/com/aes/corebackend/controller/personnelmanagement/PersonalIdentificationController.java
+++ src/main/java/com/aes/corebackend/controller/personnelmanagement/PersonalIdentificationController.java
@@ -1,12 +1,16 @@
 package com.aes.corebackend.controller.personnelmanagement;
 
 import com.aes.corebackend.dto.personnelmanagement.PersonalIdentificationInfoDTO;
+import com.aes.corebackend.dto.personnelmanagement.PersonnelManagementResponseDTO;
 import com.aes.corebackend.service.personnelmanagement.PersonalIdentificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,12 +19,18 @@ public class PersonalIdentificationController {
     private final PersonalIdentificationService service;
 
     @PostMapping(value = "/users/{userId}/identification-information")
-    public ResponseEntity<?> createAttributesInfo(@RequestBody PersonalIdentificationInfoDTO idDTO, @PathVariable Long userId) {
+    public ResponseEntity<?> createAttributesInfo(@Valid @RequestBody PersonalIdentificationInfoDTO idDTO, BindingResult result, @PathVariable Long userId) {
+        if(result.hasErrors()){
+            return ResponseEntity.ok(new PersonnelManagementResponseDTO(result.getFieldError().getDefaultMessage(), false, null));
+        }
         return ResponseEntity.ok(service.create(idDTO, userId));
     }
 
     @PutMapping(value = "/users/{userId}/identification-information")
-    public ResponseEntity<?> updateAttributesInfo(@RequestBody PersonalIdentificationInfoDTO idDTO, @PathVariable Long userId) {
+    public ResponseEntity<?> updateAttributesInfo(@Valid @RequestBody PersonalIdentificationInfoDTO idDTO, BindingResult result, @PathVariable Long userId) {
+        if(result.hasErrors()){
+            return ResponseEntity.ok(new PersonnelManagementResponseDTO(result.getFieldError().getDefaultMessage(), false, null));
+        }
         return ResponseEntity.ok(service.update(idDTO, userId));
     }
 
--- src/main/java/com/aes/corebackend/dto/personnelmanagement/PersonalIdentificationInfoDTO.java
+++ src/main/java/com/aes/corebackend/dto/personnelmanagement/PersonalIdentificationInfoDTO.java
@@ -2,15 +2,22 @@ package com.aes.corebackend.dto.personnelmanagement;
 
 import com.aes.corebackend.entity.personnelmanagement.PersonalIdentificationInfo;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Pattern;
 
 @Data
 public class PersonalIdentificationInfoDTO {
     //max length 13
     //min length 10
     //numeric
+    @Length(min = 10, max = 13, message = "NationalID: Length should be minimum 10, maximum 13 characters")
+    @Pattern(regexp = "^[0-9]+$", message = "NationalID: Field cannot have alphabetic or special characters")
     private String nationalID;
     //max length - need to check
     //numeric - need to check and confirm
+    @Length(min = 12, max = 12, message = "E-Tin Number: Length should be 12 characters")
+    @Pattern(regexp = "^[0-9]+$", message = "E-Tin: Field cannot have alphabetic or special characters")
     private String etin;// can not create attribute with single lower case first e.g eTin, pTin etc
 
     public static PersonalIdentificationInfo getPersonalIdentificationEntity(PersonalIdentificationInfoDTO identificationInfoDTO) {
