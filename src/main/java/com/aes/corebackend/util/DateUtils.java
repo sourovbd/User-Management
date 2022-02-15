a5bc131bf9b695141a7ddab26057f095914d5ba1
Jahangir Alam <jahangir.alam2@anwargroup.net>
2022-02-15 18:10:25 +0600

    date util changed

--- src/main/java/com/aes/corebackend/util/DateUtils.java
+++ src/main/java/com/aes/corebackend/util/DateUtils.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(Constants.BD_DATE_FORMAT);
+    
     public static LocalDate convertToLocalDate(String date) {
         return LocalDate.parse(date, formatter);
     }
