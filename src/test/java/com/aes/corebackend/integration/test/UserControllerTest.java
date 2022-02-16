ec8a26f94948ec160a9cef9a502654e0fadfe88f
ateqaes <atequer.rahman@anwargroup.net>
2022-02-16 10:35:22 +0600

    listener added & code refactored for fixing warning.

--- src/test/java/com/aes/corebackend/integration/test/UserControllerTest.java
+++ src/test/java/com/aes/corebackend/integration/test/UserControllerTest.java
@@ -1,12 +1,14 @@
 package com.aes.corebackend.integration.test;
 
 import com.aes.corebackend.entity.usermanagement.User;
+import com.aes.corebackend.entity.usermanagement.UserCredential;
 import com.aes.corebackend.repository.usermanagement.UserCredentialRepository;
 import com.aes.corebackend.repository.usermanagement.UserRepository;
 import com.aes.corebackend.service.springsecurity.CustomUserDetailsService;
 
 import com.aes.corebackend.util.JwtUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,7 +19,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -27,8 +34,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
+})
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
 public class UserControllerTest {
 
     @Autowired
@@ -61,10 +77,12 @@ public class UserControllerTest {
     }
     @Test
     @DatabaseSetup("/dataset/users.xml")
-    @DatabaseSetup("/dataset/user_credential.xml")
+    @DatabaseSetup("/dataset/user_credentials.xml")
     public void getAllUsers_success() throws Exception {
         List<User> users = userRepository.findAll();
         System.out.println("size: "+users.size());
+        List<UserCredential> userCredentialList = userCredentialRepository.findAll();
+        System.out.println("size: "+userCredentialList.size());
 
         mockMvc.perform(get("/users")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + TOKEN)
