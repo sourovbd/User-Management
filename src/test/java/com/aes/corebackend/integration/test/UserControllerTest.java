5a6509c7d82cf04c5335f19fd9ac870f2c69d24e
ahadulalam <ahad.alam@anwargroup.net>
2022-02-16 10:27:35 +0600

    integration test for user and user credential added

---
+++ src/test/java/com/aes/corebackend/integration/test/UserControllerTest.java
@@ -0,0 +1,93 @@
+package com.aes.corebackend.integration.test;
+
+import com.aes.corebackend.AescorebackendApplicationTests;
+import com.aes.corebackend.entity.usermanagement.User;
+import com.aes.corebackend.repository.usermanagement.UserCredentialRepository;
+import com.aes.corebackend.repository.usermanagement.UserRepository;
+import com.aes.corebackend.service.springsecurity.CustomUserDetailsService;
+
+import com.aes.corebackend.util.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static com.aes.corebackend.util.response.UMAPIResponseMessage.USER_FETCH_OK;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
+})
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+public class UserControllerTest {
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Autowired
+    private UserCredentialRepository userCredentialRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    private static String USERNAME = "012518";
+    private static String TOKEN = "";
+    private UserDetails userDetails;
+
+    @BeforeEach
+    public void setup() {
+        userDetails = userDetailsService.loadUserByUsername(USERNAME);
+        System.out.println("username:  "+userDetails.getUsername());
+        TOKEN = jwtTokenUtil.generateToken(userDetails);
+    }
+    @Test
+    @DatabaseSetup("/dataset/users.xml")
+    @DatabaseSetup("/dataset/user_credentials.xml")
+    public void getAllUsers_success() throws Exception {
+        List<User> users = userRepository.findAll();
+        System.out.println("size: "+users.size());
+
+        mockMvc.perform(get("/users")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + TOKEN)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(USER_FETCH_OK));
+    }
+
+}
