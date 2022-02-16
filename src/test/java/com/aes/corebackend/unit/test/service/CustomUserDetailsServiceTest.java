e15122dd0d7adcad63a0b5962ead7b5832ad4257
ateqaes <atequer.rahman@anwargroup.net>
2022-02-16 11:02:06 +0600

    package name changed.

--- src/test/java/com/aes/corebackend/unit/test/service/CustomUserDetailsServiceTest.java
+++
@@ -1,4 +0,0 @@
-package com.aes.corebackend.unit.test.service;
-
-public class CustomUserDetailsServiceTest {
-}
---
+++ src/test/java/com/aes/corebackend/unittest/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,4 @@
+package com.aes.corebackend.unittest.service;
+
+public class CustomUserDetailsServiceTest {
+}
