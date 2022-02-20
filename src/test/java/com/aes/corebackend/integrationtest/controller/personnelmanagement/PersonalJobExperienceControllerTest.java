aaaf7dd58597f98312f522651bfda912bbf66884
Jahangir Alam <jahangir.alam2@anwargroup.net>
2022-02-20 15:46:31 +0600

    job experience success test cases

--- src/test/java/com/aes/corebackend/integrationtest/controller/personnelmanagement/PersonalJobExperienceControllerTest.java
+++ src/test/java/com/aes/corebackend/integrationtest/controller/personnelmanagement/PersonalJobExperienceControllerTest.java
@@ -1,11 +1,8 @@
 package com.aes.corebackend.integrationtest.controller.personnelmanagement;
 
-import com.aes.corebackend.dto.personnelmanagement.PersonalFamilyInfoDTO;
 import com.aes.corebackend.dto.personnelmanagement.PersonalJobExperienceDTO;
-import com.aes.corebackend.entity.personnelmanagement.PersonalFamilyInfo;
 import com.aes.corebackend.entity.personnelmanagement.PersonalJobExperience;
 import com.aes.corebackend.entity.usermanagement.User;
-import com.aes.corebackend.repository.personnelmanagement.PersonalFamilyInfoRepository;
 import com.aes.corebackend.repository.personnelmanagement.PersonalJobExperienceRepository;
 import com.aes.corebackend.service.springsecurity.CustomUserDetailsService;
 import com.aes.corebackend.util.JwtUtil;
@@ -14,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +28,7 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import static com.aes.corebackend.util.response.PMAPIResponseMessage.*;
 import static com.aes.corebackend.util.response.APIResponseStatus.*;
@@ -65,10 +64,11 @@ public class PersonalJobExperienceControllerTest {
     @Autowired
     private PersonalJobExperienceRepository personalJobExperienceRepository;
 
-    private static String USERNAME = "012517";
+    private static String USERNAME = "012518";
     private static String TOKEN = "";
     private UserDetails userDetails;
 
+    private ObjectMapper om = new ObjectMapper();
     private APIResponse expectedResponse = APIResponse.getApiResponse();
     private PersonalJobExperienceDTO personalJobExperienceDTO = new PersonalJobExperienceDTO();
 
@@ -90,6 +90,9 @@ public class PersonalJobExperienceControllerTest {
             "responsibilities", user
     );
 
+    private PersonalJobExperienceDTO jobExperienceDTO1 = new PersonalJobExperienceDTO();
+    private PersonalJobExperienceDTO jobExperienceDTO2 = new PersonalJobExperienceDTO();
+
     @BeforeEach
     public void setup() {
         userDetails = userDetailsService.loadUserByUsername(USERNAME);
@@ -100,12 +103,28 @@ public class PersonalJobExperienceControllerTest {
         personalJobExperienceDTO.setEndDate("12-03-2018");
         personalJobExperienceDTO.setDesignation("SDE");
         personalJobExperienceDTO.setResponsibilities("responsibilities");
+
+        jobExperienceDTO1.setId(1L);
+        jobExperienceDTO1.setEmployerName("REVE");
+        jobExperienceDTO1.setStartDate("12-03-2017");
+        jobExperienceDTO1.setEndDate("12-09-2020");
+        jobExperienceDTO1.setDesignation("SDE");
+        jobExperienceDTO1.setResponsibilities("Development");
+
+        jobExperienceDTO2.setId(2L);
+        jobExperienceDTO2.setEmployerName("Nascenia");
+        jobExperienceDTO2.setStartDate("12-03-2017");
+        jobExperienceDTO2.setEndDate("12-09-2020");
+        jobExperienceDTO2.setDesignation("SDE");
+        jobExperienceDTO2.setResponsibilities("Development");
     }
 
     @Test
-//    @DatabaseSetup("/dataset/users.xml")
+    @DisplayName("job experience create - success")
+    @DatabaseSetup("/dataset/users.xml")
+    @DatabaseSetup("/dataset/user_credentials.xml")
     @DatabaseSetup("/dataset/personal_job_experience.xml")
-    public void createPersonalJobExperienceSuccessTest() throws Exception {
+    public void createJobExperienceSuccessTest() throws Exception {
 
         expectedResponse.setResponse(JOB_EXPERIENCE_CREATE_SUCCESS, TRUE, null, SUCCESS);
 
@@ -113,12 +132,74 @@ public class PersonalJobExperienceControllerTest {
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + TOKEN)
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                         .content(objectMapper.writeValueAsString(personalJobExperienceDTO)))
-                .andExpect(status().isOk());
-//                .andExpect(jsonPath("$.message").value(expectedResponse.getMessage()))
-//                .andExpect(jsonPath("$.success").value(expectedResponse.isSuccess()))
-//                .andExpect(jsonPath("$.status").value(expectedResponse.getStatus().toString()))
-//                .andExpect(jsonPath("$.data").value(expectedResponse.getData()));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(expectedResponse.getMessage()))
+                .andExpect(jsonPath("$.success").value(expectedResponse.isSuccess()))
+                .andExpect(jsonPath("$.status").value(expectedResponse.getStatus().toString()))
+                .andExpect(jsonPath("$.data").value(expectedResponse.getData()));
     }
 
+    @Test
+    @DisplayName("update job experience record - success")
+    @DatabaseSetup("/dataset/users.xml")
+    @DatabaseSetup("/dataset/user_credentials.xml")
+    @DatabaseSetup("/dataset/personal_job_experience.xml")
+    public void updateJobExperienceSuccessTest() throws Exception {
+        PersonalJobExperienceDTO existingJobExperience = PersonalJobExperienceDTO.getPersonalJobExperienceDTO(personalJobExperienceRepository.findPersonalJobExperienceById(1L));
+        existingJobExperience.setEmployerName("NewEmployerName");
 
+        expectedResponse.setResponse(JOB_EXPERIENCE_UPDATE_SUCCESS, TRUE, null, SUCCESS);
+
+        mockMvc.perform(put("/users/1/job-experiences/1")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + TOKEN)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(existingJobExperience)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(expectedResponse.getMessage()))
+                .andExpect(jsonPath("$.success").value(expectedResponse.isSuccess()))
+                .andExpect(jsonPath("$.status").value(expectedResponse.getStatus().toString()))
+                .andExpect(jsonPath("$.data").value(expectedResponse.getData()));
+    }
+
+    @Test
+    @DisplayName("read single job experience record - success")
+    @DatabaseSetup("/dataset/users.xml")
+    @DatabaseSetup("/dataset/user_credentials.xml")
+    @DatabaseSetup("/dataset/personal_job_experience.xml")
+    public void readSingleJobExperienceSuccessTest() throws Exception {
+
+        PersonalJobExperienceDTO existingJobExperience = PersonalJobExperienceDTO.getPersonalJobExperienceDTO(personalJobExperienceRepository.findPersonalJobExperienceById(1L));
+        expectedResponse.setResponse(JOB_EXPERIENCE_RECORD_FOUND, TRUE, existingJobExperience, SUCCESS);
+
+        mockMvc.perform(get("/users/1/job-experiences/1")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + TOKEN)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(expectedResponse.getMessage()))
+                .andExpect(jsonPath("$.success").value(expectedResponse.isSuccess()))
+                .andExpect(jsonPath("$.status").value(expectedResponse.getStatus().toString()))
+                .andExpect(jsonPath("$.data").value(expectedResponse.getData()));
+    }
+
+    @Test
+    @DisplayName("read multiple job experience record - success")
+    @DatabaseSetup("/dataset/users.xml")
+    @DatabaseSetup("/dataset/user_credentials.xml")
+    @DatabaseSetup("/dataset/personal_job_experience.xml")
+    public void readMultipleJobExperienceRecordsSuccessTest() throws Exception {
+        ArrayList jobExperienceList = new ArrayList<>();
+        jobExperienceList.add(jobExperienceDTO1);
+        jobExperienceList.add(jobExperienceDTO2);
+
+        expectedResponse.setResponse(JOB_EXPERIENCE_RECORD_FOUND, TRUE, jobExperienceList, SUCCESS);
+
+        mockMvc.perform(get("/users/1/job-experiences")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + TOKEN)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(expectedResponse.getMessage()))
+                .andExpect(jsonPath("$.success").value(expectedResponse.isSuccess()))
+                .andExpect(jsonPath("$.status").value(expectedResponse.getStatus().toString()))
+                .andExpect(jsonPath("$.data[0].employerName").value(jobExperienceDTO1.getEmployerName()));
+    }
 }
